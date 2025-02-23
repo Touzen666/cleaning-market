8133ab2f2cc82da41c994a9beb251f78fa2cf9b4
Bartosz Ochedowski <ochedowski.bartosz@gmail.com>
2025-02-23 18:25:40 +0100

    Set duration time for cron

--- src/app/api/getReservations/route.ts
+++ src/app/api/getReservations/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from 'next/server';
 import { getReservations } from "@/lib/getReservationService";
 
+export const maxDuration = 60;
+
 export async function GET() {
   console.log('siema tu cron!');
   const reservations = await getReservations();
